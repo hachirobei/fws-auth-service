477722c27a435def744ff385777a7e005d289b9c
Muhammad Hafizoddin Bin Roslan <muhammadhafizoddinbinroslan@ITD00MB05.local>
2023-10-04 10:28:28 +0800

    add

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ const authRoutes = require('../routes/authRoutes');
 
 const app = express();
 
-mongoose.connect('mongodb://auth-db:27017/auth', { useNewUrlParser: true, useUnifiedTopology: true });
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/auth'; 
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => {
+    console.log('Connected to MongoDB successfully!');
+})
+.catch(err => {
+    console.error('Failed to connect to MongoDB:', err.message);
+});
 
 app.use(express.json());
 app.use(authRoutes);
 
-app.listen(3001, () => {
-  console.log('Auth service started on http://localhost:3001');
+app.listen(PORT, () => {
+  console.log(`fws-auth-service running on port ${PORT}`);
 });
