eefdbab2af844ef3f94de8915db3f6f6ace4b038
hachirobei <hachirobei93@gmail.com>
2023-10-06 01:25:45 +0800

    add

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ require('../database/mangodb-auth');
 app.use(express.json());
 app.use('/api',authRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
     console.log(`fws-auth-service running on port ${PORT}`);
 });
